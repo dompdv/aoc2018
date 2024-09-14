5676e03d7f3fa3259224949dce5f8b7f37740bbe
dompdv <dompdv@yahoo.fr>
2024-09-14 08:37:22 +0200

    Day04 simplification

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -28,39 +28,27 @@ defmodule AdventOfCode.Day04 do
   # Need 2 accumulators: one for one guard shift and one for all the shifts
 
   # Start of the recursion: should be a guard shift
-  def group_by_shifts([{:begin, d, guard} | r]),
-    do: group_by_shifts(r, guard, [{:up, d, nil}], [])
+  def group_by_shifts([{:begin, _d, guard} | r]),
+    do: group_by_shifts(r, guard, [], [])
 
   # last == the current guard ID
-  # acc_guard == the accumulator for the guard shift
+  # acc_guard == the accumulator for the guard shift (minutes of sleep)
   # acc_groups == the accumulator for all the shifts
   # End of the list, we pack  the 2 accumulators up , at the same time
   def group_by_shifts([], last, acc_guard, acc_groups),
-    do: [{last, Enum.reverse(acc_guard)} | acc_groups] |> Enum.reverse()
+    do: [{last, acc_guard} | acc_groups]
 
   # Start of a shift, we pack the previous one and start a new one
-  def group_by_shifts([{:begin, d, guard} | r], last, acc_guard, acc_groups) do
-    group_by_shifts(r, guard, [{:up, d, nil}], [{last, Enum.reverse(acc_guard)} | acc_groups])
+  def group_by_shifts([{:begin, _d, guard} | r], last, acc_guard, acc_groups) do
+    group_by_shifts(r, guard, [], [{last, List.flatten(acc_guard)} | acc_groups])
   end
 
   # accumulate in the same guard shift
-  def group_by_shifts([o | r], last, acc_guard, acc_group) do
-    group_by_shifts(r, last, [o | acc_guard], acc_group)
-  end
-
-  ## Process a shift. List all the minutes when the gaurd was asleep during his shift
-  def asleep(l), do: asleep(l, [])
-
-  def asleep([], s), do: List.flatten(s)
-
-  # Consider only a :fall followed by a :up
-  def asleep([{:fall, d1, _}, {:up, d2, _} | r], s) do
+  def group_by_shifts([{:fall, d1, _}, {:up, d2, _} | r], last, acc_guard, acc_group) do
     {m1, m2} = {d1.minute, d2.minute - 1}
-    asleep(r, [Enum.to_list(m1..m2) | s])
-  end
 
-  # Else go to the next line
-  def asleep([_ | r], s), do: asleep(r, s)
+    group_by_shifts(r, last, [Enum.to_list(m1..m2) | acc_guard], acc_group)
+  end
 
   def parse_and_process(args) do
     # Parse
@@ -71,8 +59,10 @@ defmodule AdventOfCode.Day04 do
     |> Enum.sort(fn {_, d1, _}, {_, d2, _} -> NaiveDateTime.compare(d1, d2) == :lt end)
     # Group by shifts
     |> group_by_shifts()
-    # Process each shift
-    |> Enum.map(fn {guard, group} -> {guard, asleep(group)} end)
+    # Regroup all the shifts of each guard and consolidate the list of sleeping minutes for each guard
+    |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
+    |> Enum.map(fn {g, l} -> {g, List.flatten(l)} end)
+    |> Map.new()
   end
 
   def part1(args) do
@@ -83,21 +73,12 @@ defmodule AdventOfCode.Day04 do
       groups
       # Consider the time spent sleeping
       |> Enum.map(fn {guard, group} -> {guard, Enum.count(group)} end)
-      # Regroup all the shifts for each guard
-      |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
-      # For each guard, sum  the total time sleeping
-      |> Enum.map(fn {k, l} -> {k, Enum.sum(l)} end)
       # Find the guard that sleeps the most
       |> Enum.max_by(&elem(&1, 1))
 
     # Find the minute the sleepy guard sleeps the most
     {most_minute, _} =
-      groups
-      # Keep only the shifts of the sleepy guard
-      |> Enum.filter(&(elem(&1, 0) == sleepy_guard))
-      # Create a list of all the sleeping minutes over all shifts
-      |> Enum.map(&elem(&1, 1))
-      |> List.flatten()
+      groups[sleepy_guard]
       # Identify the most frequent minute
       |> Enum.frequencies()
       |> Enum.max_by(&elem(&1, 1))
@@ -108,9 +89,6 @@ defmodule AdventOfCode.Day04 do
   def part2(args) do
     {guard, {max_minute, _}} =
       parse_and_process(args)
-      # Regroup all the shifts of each guard and consolidate the list of sleeping minutes for each guard
-      |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
-      |> Enum.map(fn {g, l} -> {g, l |> List.flatten()} end)
       # Do not consider the guards who never sleep
       |> Enum.filter(&(elem(&1, 1) != []))
       # Select, for each guard, the most frequent minute
