df59bf6b0ba26638df35f2c7b247e3eacd9e16ff
dompdv <dompdv@yahoo.fr>
2024-09-18 23:59:17 +0200

    Day09 P1 & P2 Final

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -4,46 +4,82 @@ defmodule AdventOfCode.Day09 do
     {String.to_integer(a), String.to_integer(b)}
   end
 
-  def put({ring, ring_size, current_marble}, marble) when rem(marble, 23) == 0 do
-    n =
-      if current_marble - 7 < 0, do: current_marble - 7 + ring_size, else: current_marble - 7
+  # The Ring is modeled as a Map. Each marble knows the previous and next marble in the ring
+  # { %{a_marble => {previous_marble, next_marble}}, current_marble}
+  #
 
-    left = binary_part(ring, 0, n * 4)
-    <<removed::big-unsigned-integer-size(32)>> = binary_part(ring, n * 4, 4)
-    right = binary_slice(ring, (n * 4 + 4)..-1//1)
-    {{left <> right, ring_size - 1, n}, removed + marble}
+  # Initial Ring
+  def new_ring(), do: {%{0 => {0, 0}}, 0}
+
+  def move_clockwise(ring, current_marble), do: ring[current_marble] |> elem(1)
+
+  # Counter clockwise
+  def move_cclockwise(ring, current_marble), do: ring[current_marble] |> elem(0)
+
+  def move_cclockwise(ring, current_marble, n) do
+    Enum.reduce(1..n, current_marble, fn _, m -> move_cclockwise(ring, m) end)
   end
 
-  def put({ring, ring_size, current_marble}, marble) do
-    case rem(current_marble + 2, ring_size) do
-      0 ->
-        {{ring <> <<marble::unsigned-big-integer-size(32)>>, ring_size + 1, ring_size}, 0}
+  # Case where the marble is a multiple of 23
+  def put({ring, current_marble}, marble) when rem(marble, 23) == 0 do
+    # Find the marble 7 steps counter clockwise
+    m7_marble = move_cclockwise(ring, current_marble, 7)
+    # Identify its neighbors
+    {p_marble, n_marble} = ring[m7_marble]
+    # Store the previous marble on the left and the next marble of the right
+    {p_marble_previous, _} = ring[p_marble]
+    {_, n_marble_next} = ring[n_marble]
+    # Remove the m7_marble
+    new_ring =
+      ring
+      |> Map.put(p_marble, {p_marble_previous, n_marble})
+      |> Map.put(n_marble, {p_marble, n_marble_next})
+      |> Map.delete(m7_marble)
 
-      n ->
-        left = binary_part(ring, 0, n * 4)
-        right = binary_slice(ring, (n * 4)..-1//1)
-        {{left <> <<marble::unsigned-big-integer-size(32)>> <> right, ring_size + 1, n}, 0}
-    end
+    # Increase the score
+    {{new_ring, n_marble}, marble + m7_marble}
   end
 
-  def new_ring(), do: {<<0::unsigned-big-integer-size(32)>>, 1, 0}
+  # Dealing with the initial case
+  def put({%{0 => {0, 0}}, 0}, 1) do
+    {{%{0 => {1, 1}, 1 => {0, 0}}, 1}, 0}
+  end
 
+  # Standard case
+  def put({ring, current_marble}, marble) do
+    # Identify the 2 next marbles clockwise
+    p1_marble = move_clockwise(ring, current_marble)
+    p2_marble = move_clockwise(ring, p1_marble)
+
+    # Insert marble
+    new_ring =
+      ring
+      |> Map.put(p1_marble, {current_marble, marble})
+      |> Map.put(marble, {p1_marble, p2_marble})
+      |> Map.put(p2_marble, {marble, ring[p2_marble] |> elem(1)})
+
+    {{new_ring, marble}, 0}
+  end
+
+  # Main game loop
   def play({n_players, last_marble}) do
+    # Initialize the players' score
     initial_scores = for p <- 0..(n_players - 1), into: %{}, do: {p, 0}
 
     Enum.reduce(
       1..last_marble,
+      # {ring  state, current player id, scores}
       {new_ring(), 0, initial_scores},
       fn m, {r, p, scores} ->
-        if rem(m, 1000) == 0, do: IO.inspect(m)
-
+        # Put a marble in the ring
         {new_r, score} = put(r, m)
-
+        # Update scores if necessary
         if score == 0,
           do: {new_r, rem(p + 1, n_players), scores},
           else: {new_r, rem(p + 1, n_players), Map.update!(scores, p, fn s -> s + score end)}
       end
     )
+    # Find the maximum score
     |> elem(2)
     |> Map.values()
     |> Enum.max()
