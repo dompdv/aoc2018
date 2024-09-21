9a0d51d4cda60ceaf369c1a22a8d6c1b3b5b0da2
dompdv <dompdv@yahoo.fr>
2024-09-21 23:31:09 +0200

    Day10, refactored after discussion with olb17

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -7,41 +7,26 @@ defmodule AdventOfCode.Day10 do
 
   def parse(args), do: args |> String.split("\n", trim: true) |> Enum.map(&parse_line/1)
 
-  def get_x(stars) do
-    for {x, _} <- stars, do: x
-  end
-
-  def get_y(stars) do
-    for {_, y} <- stars, do: y
-  end
+  def get_x(stars), do: Enum.map(stars, &elem(&1, 0))
+  def get_y(stars), do: Enum.map(stars, &elem(&1, 1))
 
   def print(stars) do
     {x_min, x_max} = stars |> get_x |> Enum.min_max()
     {y_min, y_max} = stars |> get_y |> Enum.min_max()
 
-    if x_max - x_min > 300 or y_min - y_min > 30 do
-      "Too big"
-    else
-      stars = MapSet.new(stars)
-
-      for y <- y_min..y_max do
-        for x <- x_min..x_max do
-          if {x, y} in stars, do: "*", else: " "
-        end
-        |> Enum.join()
-        |> IO.puts()
+    for y <- y_min..y_max do
+      for x <- x_min..x_max do
+        if {x, y} in stars, do: "*", else: " "
       end
+      |> Enum.join()
+      |> IO.puts()
     end
-
-    nil
   end
 
-  # Measures the total of distinct x's and y's in the list
+  # Count the total of distinct y's in the list
   def dispers(stars) do
-    dispers_x = stars |> get_x() |> Enum.uniq() |> length()
-    dispers_y = stars |> get_y() |> Enum.uniq() |> length()
-
-    dispers_x + dispers_y
+    {y_min, y_max} = stars |> get_y |> Enum.min_max()
+    y_max - y_min
   end
 
   # Compute the star positions at time t
@@ -52,16 +37,14 @@ defmodule AdventOfCode.Day10 do
   def part1(args) do
     starvec = args |> parse()
 
-    # Find the time of minimal dispersion of x's and y's
-    {target_t, _} =
-      for t <- 1..12000 do
-        {t, starvec |> at_time(t) |> dispers()}
-      end
-      |> Enum.min_by(&elem(&1, 1))
+    # Iterate until the y's dispersion <= 10
+    [target_t] =
+      Stream.iterate(0, &(&1 + 1))
+      |> Stream.filter(fn t -> dispers(at_time(starvec, t)) <= 10 end)
+      |> Enum.take(1)
 
-    # This is when the message appears
     IO.puts("Message appears at time #{target_t}")
-    starvec |> at_time(target_t) |> print()
+    print(at_time(starvec, target_t))
   end
 
   def part2(args), do: part1(args)
