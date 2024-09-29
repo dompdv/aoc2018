a3f24b27a06d07e34be43e16d866efd26a6f7421
dompdv <dompdv@yahoo.fr>
2024-09-29 11:05:00 +0200

    Day14 P1&P2 (P2 does not work)

--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,62 +1,51 @@
 defmodule AdventOfCode.Day14 do
-  def parse(args), do: args |> String.trim() |> String.to_integer()
-
+  # One turn
   def cook({recipes, elf1, elf2}) do
+    # New recipe score
     v = recipes[elf1] + recipes[elf2]
-    last_index = map_size(recipes)
+    size = map_size(recipes)
+    # Split units and "dizaines"
+    units = rem(v, 10)
+    diz = div(v, 10)
 
-    {recipes, last_index} =
-      if div(v, 10) > 0,
-        do: {Map.put(recipes, last_index, div(v, 10)), last_index + 1},
-        else: {recipes, last_index}
+    # Add them at the end of the map
+    new_recipes =
+      if diz > 0,
+        do: recipes |> Map.put(size, diz) |> Map.put(size + 1, units),
+        else: recipes |> Map.put(size, units)
 
-    {recipes, last_index} = {Map.put(recipes, last_index, rem(v, 10)), last_index + 1}
-    elf1 = rem(elf1 + recipes[elf1] + 1, last_index)
-    elf2 = rem(elf2 + recipes[elf2] + 1, last_index)
-    {recipes, elf1, elf2}
+    # Adjust the elves' positions
+    new_size = map_size(new_recipes)
+    new_elf1 = rem(elf1 + new_recipes[elf1] + 1, new_size)
+    new_elf2 = rem(elf2 + new_recipes[elf2] + 1, new_size)
+    {new_recipes, new_elf1, new_elf2}
   end
 
+  # The state is {a map of the recipes' scores, position of elf 1, position of elf 2}
   def initial_state(), do: {%{0 => 3, 1 => 7}, 0, 1}
 
   def part1(args) do
-    rounds = args |> parse()
+    rounds = args |> String.trim() |> String.to_integer()
 
-    {r, _, _} = Enum.reduce(1..rounds, initial_state(), fn _, s -> cook(s) end)
+    # Take some margin (number of rounds to compute to be sure)
+    {r, _, _} = Enum.reduce(1..(rounds * 2), initial_state(), fn _, s -> cook(s) end)
     for(s <- rounds..(rounds + 9), do: Integer.to_string(r[s])) |> Enum.join()
   end
 
-  def print({r, e1, e2} = s, n) do
-    for i <- (map_size(r) - n)..(map_size(r) - 1) do
-      cond do
-        i == e1 -> "(#{r[i]}) "
-        i == e2 -> "[#{r[i]}] "
-        true -> " #{r[i]}  "
-      end
-    end
-    |> Enum.join()
-    |> IO.puts()
-
-    s
-  end
-
   def part2(args) do
-    args = "59414"
     target = args |> String.trim() |> to_charlist() |> Enum.map(&(&1 - ?0))
     len_target = length(target)
 
     Stream.iterate(0, &(&1 + 1))
     |> Enum.reduce_while(
       initial_state(),
-      fn i, s ->
-        new_s = cook(s)
-        r = elem(new_s, 0)
+      fn _i, s ->
+        {r, _, _} = new_s = cook(s)
         t = map_size(r)
         tail = for i <- (t - len_target)..(t - 1), do: r[i]
         if tail == target, do: {:halt, new_s}, else: {:cont, new_s}
       end
     )
-    |> elem(0)
-    |> map_size()
-    |> then(&(&1 - len_target))
+    |> then(fn {r, _, _} -> map_size(r) - len_target end)
   end
 end
